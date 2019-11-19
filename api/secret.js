15adab53db158a7d4c97eec42a5d911fe0aef341
Dale Pancho <dalepancho7@gmail.com>
2019-11-19 14:30:58 -0800

    usersRoute done except login. Password hashes

---
+++ api/secret.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+      jwtSecret: process.env.JWT_SECRET || "all your passwords are belong to us"
+    },
+    testing: {
+      jwtSecret: process.env.JWT_SECRET || "all your passwords are belong to us"
+    },
+    production: {
+      jwtSecret: process.env.JWT_SECRET || "all your passwords are belong to us"
+    }
+  };
--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const server = express()
-const serverConfig = require('./serverConfig.js')
+const serverConfig = require('./serverConfig')
+const authRoute = require('./routes/usersRouter');
 
 server.use(express.json())
 serverConfig(server)
+
+server.use('/api/auth', authRoute); //localhost:4444/api/auth
 
 module.exports = server;
